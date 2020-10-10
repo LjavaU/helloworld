7f3fdfdbaab596051e2aaa0a92a22d189e0b0df3
luhao <lh070960@gmail.com>
2020-10-10 09:41:15 +0800

    修改测试类

--- git_day01/src/main/java/com/itheima/domain/Test.java
+++ git_day01/src/main/java/com/itheima/domain/Test.java
@@ -1,8 +1,13 @@
 package com.itheima.domain;
 
+import java.sql.SQLOutput;
+
 /**
  * @Author: 陆浩
  * @Date: 2020/10/9 20:56
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
 }
